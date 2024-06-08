020d961eaef3c6897d96aac824fa3a755d470fa5
tomason182 <elotro182@hotmail.com>
2024-06-08 15:39:24 -0300

    add style to skills

--- src/components/ResumeSkills.jsx
+++ src/components/ResumeSkills.jsx
@@ -2,20 +2,26 @@ export default function ResumeSkills() {
   return (
     <div className="skills">
       <h2>Skills</h2>
-      <ul className="hard-skills">
-        <li>Basic Swordsmanship</li>
-        <li>Patrolling and Surveillance</li>
-        <li>Military Administration</li>
-        <li>Culinary Skills</li>
-        <li>Basic First Aid</li>
-      </ul>
-      <ul className="soft-skills">
-        <li>Empathy and Friendliness</li>
-        <li>Perseverance</li>
-        <li>Conflict Resolution</li>
-        <li>Communication</li>
-        <li>Loyalty</li>
-      </ul>
+      <div className="hard-skills">
+        <h3>Hard Skills</h3>
+        <ul>
+          <li>Basic Swordsmanship</li>
+          <li>Patrolling and Surveillance</li>
+          <li>Military Administration</li>
+          <li>Culinary Skills</li>
+          <li>Basic First Aid</li>
+        </ul>
+      </div>
+      <div className="soft-skills">
+        <h3>Soft Skills</h3>
+        <ul>
+          <li>Empathy and Friendliness</li>
+          <li>Perseverance</li>
+          <li>Conflict Resolution</li>
+          <li>Communication</li>
+          <li>Loyalty</li>
+        </ul>
+      </div>
     </div>
   );
 }
